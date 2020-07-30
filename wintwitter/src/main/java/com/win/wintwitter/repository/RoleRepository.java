cc4a56ebebd4029c8eba758c1a60539b15a85273
Aabha <aabha.khandwala@gmail.com>
2020-07-30 10:35:25 -0400

    Twitter clone project

---
+++ wintwitter/src/main/java/com/win/wintwitter/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.win.wintwitter.repository;
+
+import com.win.wintwitter.model.Role;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    Role findByRole(String role);
+
+}
